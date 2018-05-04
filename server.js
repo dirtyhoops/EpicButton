f70cebf2a08fcdc6a7e64454badf3d09fd750837
daryll <dosis@csumb.edu>
2018-05-03 18:25:37 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+const server = app.listen(8000);
+var io = require('socket.io').listen(server);
+var count = 0;
+io.sockets.on('connection', function(socket) {
+    socket.on( "count", function(){
+        count++;
+        socket.emit('serverResponse', count);
+    });
+    
+    socket.on( "clear", function(){
+        count = 0;
+        socket.emit('serverResponse', count);
+    });  
+
+})
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/static'));
+
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+
+
+
+
+
